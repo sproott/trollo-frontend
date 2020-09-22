b189551515aa0dc3bde99f6c185b96480fc17fda
sproott <david.hrabe12@seznam.cz>
2020-09-22 21:43:18 +0200

    Improved loading page

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,21 +1,24 @@
 import React from "react"
-import { Header } from "./common/page.styled"
+import { Header, Layout } from "./common/page.styled"
 import { Spin } from "antd"
 import Box from "./common/Box"
 import { H1 } from "./common/Text"
 
 const LoadingPage = () => {
   return (
-    <>
-      <Box style={{ minHeight: "100%" }} flex flexDirection="column">
-        <Header>
-          <H1 color="white">Trollo</H1>
-        </Header>
-        <Box style={{ flexGrow: 1 }} flex justifyContent="center" alignItems="center">
-          <Spin size="large" tip="Loading..." />
-        </Box>
+    <Layout style={{ minHeight: "100%", display: "flex", flexDirection: "column" }}>
+      <Header>
+        <H1 color="white">Trollo</H1>
+      </Header>
+      <Box
+        style={{ backgroundColor: "white", flexGrow: 1 }}
+        flex
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Spin size="large" tip="Loading..." />
       </Box>
-    </>
+    </Layout>
   )
 }
 
